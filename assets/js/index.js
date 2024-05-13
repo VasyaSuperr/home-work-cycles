955b8848c75eabc1cea2b5aaf0436aa099c4a2a6
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-13 19:12:18 +0300

    Task with a while cycle

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,38 @@
+console.log("==========================Cycle while==========================");
+console.log("// 1");
+let firstNumber11 = 25;
+const lastNumber11 = 0;
 
+while (firstNumber11 >= lastNumber11) {
+  console.log("Current number: ", firstNumber11--);
+}
+
+console.log("// 2");
+let firstNumber12 = 10;
+const lastNumber12 = 50;
+
+while (firstNumber12 <= lastNumber12) {
+  console.log("The current number that is divisible by 5: ", firstNumber12);
+  firstNumber12 += 5;
+}
+
+console.log("--------------------------------------------------------------");
+firstNumber12 = 10;
+
+while (firstNumber12 <= lastNumber12) {
+  if (firstNumber12 % 5 === 0) {
+    console.log("The current number that is divisible by 5: ", firstNumber12);
+  }
+  ++firstNumber12;
+}
+
+console.log("// 3");
+let firstNumber13 = 1;
+const lastNumber13 = 100;
+let sum = null;
+
+while (firstNumber13 <= lastNumber13) {
+  sum += firstNumber13++;
+  // console.log("Suma: ", sum);
+}
+console.log("Suma: ", sum);
